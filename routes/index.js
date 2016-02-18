a1ac87ef206c7be6a556ae954dfd30b8909d8f7a
Cyrus Zei <cyruszei@gmail.com>
2016-02-18 13:44:33 +0100

    latest done

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var fs = require('fs');
 router.get('/', function(req, res, next) {
   var file = __dirname + '/../public/list/list.json';
 
-  fs.readFile(file, 'utf8', function (err, data) {
+  fs.readFile(file, 'utf8', function(err, data) {
     if (err) {
       console.log('Error: ' + err);
       return;
@@ -15,8 +15,17 @@ router.get('/', function(req, res, next) {
 
     lista = JSON.parse(data);
     console.log(lista);
-    res.render('index', { title: lista.Year.top2011.top40.id.top01.album_cover });
-});
+    res.render('index', {
+      album_cover: lista.Year.top2011.top40.id.top01.album_cover,
+      artist_namn: lista.Year.top2011.top40.id.top01.artist_namn,
+      song: lista.Year.top2011.top40.id.top01.artist_namn,
+      spotify: lista.Year.top2011.top40.id.top01.spotify,
+      youtube: lista.Year.top2011.top40.id.top01.youtube
+
+
+
+    });
+  });
 });
 
 module.exports = router;
