5580cea01924582f6dd9ae032af6c40392568f34
Cyrus Zei <cyruszei@gmail.com>
2016-02-18 09:50:07 +0100

    done

--- routes/index.js
+++ routes/index.js
@@ -6,23 +6,17 @@ var fs = require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var file = __dirname + '/../public/list/list.json';
-  var data;
+
   fs.readFile(file, 'utf8', function (err, data) {
     if (err) {
       console.log('Error: ' + err);
       return;
     }
 
-    data = JSON.parse(data);
-    console.log(data);
-    res.render('index', { title: data });
-
-
-  });
-
-
-
-
+    lista = JSON.parse(data);
+    console.log(lista);
+    res.render('index', { title: lista.Year.top2011.top40.id.top01.album_cover });
+});
 });
 
 module.exports = router;
